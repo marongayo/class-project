a991a887cadb25cc4f023c96614990b6cbe48338
marongayo <sir.ongayo@gmail.com>
2023-02-15 22:00:14 +0300

    new updates

--- dynamics.R
+++ dynamics.R
@@ -2,6 +2,7 @@ library(ggplot2)
 
 custom.col <- c("#FFDB6D", "#C4961A", "#F4EDCA", 
                          "#D16103", "#C3D7A4", "#52854C", "#4E84C4", "#293352")
+grey.scale <-c('#353535','#4F4F4F','#9A9A9A','#8A897C')
                          
 ggplot(dynamics, aes(x=HS_Head)) + 
   geom_bar(stat="count", fill=c('#0000FF','#7EC8E3')) + 
@@ -9,13 +10,16 @@ ggplot(dynamics, aes(x=HS_Head)) +
   theme(plot.title = element_text(hjust = 0.5))
   
 table(dynamics$HS_Head)
+colnames(dynamics)
 #head(dynamics)
+
 dynamics$house_type <- factor(dynamics$house_type)
+hist(dynamics$rentPrice)
 
-
-
-
-
+ggplot(dynamics, aes(x=rentPrice))+
+  geom_histogram(bins=20,fill='#1b4332', color='white')+
+  labs(title = "Histogram of Rent Prices", x = "Rent Prices", y = "Frequency")+
+  theme(plot.title = element_text(hjust = 0.5))
 
 
 
